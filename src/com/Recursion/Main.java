ae21fe52d5f8d2fc91ad747e943ff0ab04f71419
Nitin <nsinghboss02@gmail.com>
2022-03-18 23:28:46 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,31 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Treenode <Integer> root = new Treenode<>(1);
+        Treenode<Integer> node1 = new Treenode<>(2);
+        Treenode<Integer> node2 = new Treenode<>(3);
+        Treenode<Integer> node3 = new Treenode<>(4);
+        Treenode<Integer> node4 = new Treenode<>(5);
+        Treenode<Integer> node5 = new Treenode<>(6);
+        Treenode<Integer> node6 = new Treenode<>(7);
+        root.children.add(node1);
+        root.children.add(node2);
+        root.children.add(node3);
+        node1.children.add(node4);
+        node2.children.add(node5);
+        node3.children.add(node6);
+        System.out.println(nodeCount(root));
+    }
+    public static int nodeCount(Treenode<Integer> root){
+        if(root==null){
+            return 0;
+        }
+        int count = 1;
+        for(int i=0;i<root.children.size();i++){
+            count += nodeCount(root.children.get(i));
+        }
+        return count;
+    }
+}
